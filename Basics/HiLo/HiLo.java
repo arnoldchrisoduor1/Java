645ef74f36c540190e2453fb54275d6f60afdfe1
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-06-01 14:40:03 +0300

    Guessing game

---
+++ Basics/HiLo/HiLo.java
@@ -0,0 +1,22 @@
+package Basics.HiLo;
+import java.util.Scanner;
+
+public class HiLo {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        // Creating a random number for the user to guess.
+        int theNumber = (int)(Math.random() * 100 + 1);
+        int guess = 0;
+        while (guess != theNumber) {
+            System.out.println("Guess a number between 1 and 100");
+            guess = scan.nextInt();
+            if (guess < theNumber)
+                System.out.println(guess + " is too low. Tr again.");
+            else if (guess > theNumber) 
+                System.out.println(guess + " is too high. Try again");
+            else
+                System.out.println(guess + " is correct. You win!");
+        }   // End of while loop for guessing.
+    }
+}
