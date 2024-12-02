58e152b8609443802617a4b01ba2aae743b34c63
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-12-03 01:31:46 +0300

    java arrays

---
+++ Arrays/arrayOfObjects.java
@@ -0,0 +1,32 @@
+public class arrayOfObjects {
+    class Student {
+        public int roll_no;
+        public String name;
+
+        Student(int roll_no, String name) {
+            this.roll_no = roll_no;
+            this.name = name;
+        }
+    }
+
+    public class  Main {
+        public static void main(String[] args) {
+            // declaring an array of student.
+            Student[] arr;
+
+            // allocating memory of 5 objects of type student.
+            arr = new Student[5];
+
+            // initializing elements of the array.
+            arr[0] = new Student(1, "aman");
+            arr[1] = new Student(2, "vaibhav");
+            arr[2] = new Student(3, "shikar");
+            arr[3] = new Student(4, "dharmesh");
+            arr[4] = new Student(4, "dharmesh");
+            arr[5] = new Student(5, "mohit");
+
+            for(int i = 0; i < arr.length; i++)
+                System.out.println("Element at " + i + " : { " + arr[i].name+ "}");
+        }
+    }
+}
---
+++ Arrays/printArray/printArray.java
@@ -0,0 +1,11 @@
+public class printArray {
+    public static void main(String[] args)
+    {
+        int arr[] = { 1, 2, 3, 4, 5 };
+
+        int len = arr.length;
+
+        for(int i = 0; i < len; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
