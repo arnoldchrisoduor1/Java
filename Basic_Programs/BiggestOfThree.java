d97bb05c3e0d08be54e8a38770cd02c269e3daf2
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-02-05 08:44:14 +0300

    The biggest of three

--- Basic_Programs/BiggestOfThree.java
+++ Basic_Programs/BiggestOfThree.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.lang.*;
+
 public class BiggestOfThree {
     // tertiary method to finding the largest of three numbers.
     static int tertaryMethod(int x, int y, int z) {
@@ -12,6 +15,18 @@ public class BiggestOfThree {
             return y;
         else
             return y;
+    }
+    // Using the Math.max()
+    static int sumMathMethod(int x, int y, int z) {
+        return Math.max(Math.max(x, y), z);
+    }
+    // Using the collections method.
+    static int collectionsMethod(int x, int y, int z) {
+        ArrayList<Integer> p = new ArrayList<>();
+        p.add(x);
+        p.add(y);
+        p.add(z);
+        return Collections.max(p);
     }
 
     public static void main(String[] args) {
@@ -30,6 +45,14 @@ public class BiggestOfThree {
         // calling the if-else method.
         largest = ifelseMethod(a, b, c);
         // Printing out the result.
+        System.out.println(largest + " is the largest number.");
+
+        // Using the Math.max() method.
+        largest = sumMathMethod(a, b, c);
+        System.out.println(largest + " is the largest number.");
+
+        // Using the collections method.
+        largest = collectionsMethod(a, b, c);
         System.out.println(largest + " is the largest number.");
     }
 }
