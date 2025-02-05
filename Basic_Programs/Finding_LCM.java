1d70f338d0b4116f93a782bc59538f0b5dcf3e70
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-02-05 09:19:07 +0300

    finsing the LCM of numbers

--- Basic_Programs/Finding_LCM.java
+++ Basic_Programs/Finding_LCM.java
@@ -10,9 +10,19 @@ public class Finding_LCM {
     }
     System.out.println("LCM of " + a + " and " + b + " : " + largest);
    }
-
+   // Method by finding the greatest common divisor.
+   static int greatesCD(int a, int b) {
+        if (a == 0)
+            return b;
+        return greatesCD(b % a, a);
+   }
+    // LCM of two numbers.
+    static int LCM(int a, int b) {
+        return (a / greatesCD(a, b)) * b;
+    }
    public static void main(String[] args) {
     int a = 15, b = 25;
     findLCM(a, b);
+    System.out.println("LCM of " + a + " and " + b + " is " + LCM(a, b));
    }
 }
