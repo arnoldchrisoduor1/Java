f0a412d30117471f5b77ebb7e04ca9f96779cda3
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-02-10 07:35:30 +0300

    another iteration method

--- WickedCoolJava/ForLoopEnhancements.java
+++ WickedCoolJava/ForLoopEnhancements.java
@@ -21,10 +21,17 @@ public class ForLoopEnhancements {
         System.out.println("The sum " + sum);
 
         // Using the modern way to do iteration.
-        sum = 0;
-        for (Integer item : theList) {
-            sum = sum + item;
+        // sum = 0;
+        // for (Integer item : theList) {
+        //     sum = sum + item;
+        // }
+        // System.out.println("Adding using the modern example. " + sum);
+
+        int[] newList = new int[] { 2, 3, 4, 7 };
+        int newSum = 0;
+        for (int x : newList) {
+            newSum = newSum + x;
         }
-        System.out.println("Adding using thr modern example. " + sum);
+        System.out.pritnln("The sum is " + newSum);
     }
 }
